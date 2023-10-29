89e925dc52a51b80ae9804723067c0b568398932
israncho <isragutierrez@ciencias.unam.mx>
2023-10-29 16:03:49 -0600

    reporting errors

--- src/http_client.py
+++ src/http_client.py
@@ -1,17 +1,32 @@
 import socket
 from sys import argv
 
+
+HTTP_M = ['GET', 'HEAD']
+CONNECT = ['keep-alive', 'close', 'upgrade']
+ENCODING = ['gzip', 'deflate', 'identity']
+
+
 def process_args() -> dict:
     assert len(argv) == 7, 'Not enough arguments'
     arguments = dict()
-    arguments['host'] = argv[1]
+    arguments['host_server'] = argv[1]
+
+    assert argv[2] in HTTP_M, 'unavailable HTTP method, try: ' + str(HTTP_M)
     arguments['http_method'] = argv[2]
+
     arguments['url'] = argv[3]
-    user_agent = int(argv[4])
-    assert 1 <= user_agent and user_agent <= 3, 'Wrong user agent'
-    arguments['user_agent'] = user_agent
+
+    user_a = int(argv[4])
+    assert 1 <= user_a and user_a <= 3, 'Wrong user agent'
+    arguments['user_agent'] = user_a
+
+    assert argv[5] in ENCODING, 'unavailable encoding, try:' + str(ENCODING)
     arguments['encoding'] = argv[5]
+
+    assert argv[6] in CONNECT, 'unavailable connection arg, try:' + str(CONNECT)
     arguments['connection'] = argv[6]
+
     return arguments
 
 
